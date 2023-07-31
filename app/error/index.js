875f7d1ccaba76016e4c48fab5c5136488314fd2
Sadman95 <mail.sakib.1995@gmail.com>
2023-07-31 14:33:24 +0600

    fix in error handler

--- app/error/index.js
+++ app/error/index.js
@@ -4,8 +4,8 @@ const localErrorHandler = async(_req, _res, next) => {
     next(error)
 }
 
-const globalErrorHandler = async(error, _req, res) => {
-    if(error){
+const globalErrorHandler = async(error, _req, res, _next) => {
+    if(error.status){
         return res.status(error.status).json({message: error.message})
     }else{
         return res.status(500).json({message: "Something went wrong"})
